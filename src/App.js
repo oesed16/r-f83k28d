73b0ecc3f11d8c786531c250bb8fcada7547cacb
Angela Castagno <oesed16@gmail.com>
2018-12-19 02:16:42 -0500

    Reto Incrementa el número

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1 
+    });
+  }
   render() {
     return (
+
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button id='inc' onClick={this.increment}>Incrementa</button>
       </div>
     );
   }
